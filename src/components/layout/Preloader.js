bffd042ab138ce9ec2ef1c1077725c1b9aab26a4
TegHocine <arcou98@hotmail.com>
2021-12-10 15:42:53 +0100

    Initial commit

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Preloader = () => {
+  return (
+    <div className='progress teal lighten-4'>
+      <div className='indeterminate teal' />
+    </div>
+  );
+};
+
+export default Preloader;
